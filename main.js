4b0e11ba479b7dca8503764f57cb4ebef8555500
Shakzod Muradov <shaxzod1996shax@gmail.com>
2022-04-01 08:58:47 +0500

    insta

---
+++ main.js
@@ -0,0 +1,427 @@
+let arr = [
+  {
+    id: 1,
+    title: "Najot Talim",
+    name: "Najot Talim",
+    media: [
+      "../../img/nature1.jpg",
+      "../../video/first.mp4",
+      "../../img/firts1.jpg",
+      "../../img/first2.jpg",
+    ],
+    desc: `«Najot Ta'lim» markazida Foundation dasturlash bosqichida ta'lim olayotgan o'quvchilar uchun bir necha kun avval mutaxassislik kursiga o'tish imtihoni bo'lib o'tgan edi.<br> Mantiq va algoritmlash bo'yicha bo'lib o'tgan imtihonda 162 nafar o'quvchilar keyingi bosqichga o'tish uchun harakat qilib ko'rdilar. <br> Ushbu o'tish imtihonida eng yuqori ball to'plagan 9 nafar o'quvchiga, «Najot Ta'lim» markazi tomonidan istalgan bitta mutaxassislik kursida o'qish uchun chegirmalar taqdim qilinadi.`,
+    likeNumber: 20,
+    comentNumber: 13,
+    save: false,
+  },
+
+  {
+    id: 2,
+    title: "SHaxzod Muradov",
+    name: "Najot Talim",
+    media: [
+      "../../video/fifth.mp4",
+      "../../img/nature2.jpg",
+      "../../img/second1.jpg",
+    ],
+    desc: `O'zbekistondagi xususiy ta'lim salohiyatini yanada rivojlantirish maqsadida EduOn va Oliygoh.uz hamkorligida "Edu-Branding" tanlovi e'lon qilingan edi. <br> Tanlov natijalariga ko'ra, 3257 ta ovoz bilan «Najot Ta'lim» markazi «Eng yaxshi IT maktab» nominatsiyasining g'olibi deb topildi. <br> Ushbu tanlovda «Najot Ta'lim» markaziga ishonch bildirib, o'z ovozini bergan insonlarga va tanlov tashkilotchilariga jamoamiz nomidan samimiy minnatdorchilik bildiramiz. <br> 😊 🚀 Siz tomoningizdan bildirilgan ushbu ishonchni oqlash uchun doimo harakat qilamiz.`,
+    likeNumber: 47,
+    comentNumber: 22,
+    save: false,
+  },
+
+  {
+    id: 3,
+    title: "Shukurov Elyorjon",
+    name: "Najot Talim",
+    media: [
+      "../../img/nature3.jpg",
+      "../../video/second.mp4",
+      "../../img/thirs1.jpg",
+      "../../img/third2.jpg",
+    ],
+    desc: `Tipografika nima? Grafik dizayn sohasida ishlatiladigan bu so'zni umumiy ma'noda yozuv shriftlari bilan samarali ishlash, deb aytsak bo'ladi.<br> Ya'ni dizaynda mavzuga aloqador shrift turi, o'lchami va joylashuvini to'g'ri tanlash san'atidir. <br> 📌 Chunki matn ma'lumot uzatishning asosiy vositalaridan biri hisoblanadi. <br> Shu sababli har qanday dizaynda tipografikaning to'g'ri tanlanganligi muhim sanaladi. <br>Yuqorida tipografikaga oid foydali ma'lumotni qoldirdik. 😉 📞 +998712001123`,
+    likeNumber: 220,
+    comentNumber: 58,
+    save: false,
+  },
+
+  {
+    id: 4,
+    title: "Ashurov Rasul",
+    name: "Najot Talim",
+    media: [
+      "../../img/nature4.webp",
+      "../../video/first.mp4",
+      "../../img/fourth1.jpg",
+    ],
+    desc: `Men Najot Ta’lim jamoasiga birgaliklari uchun, ta’limni rivojlantirishda. <br> Qo’shayotgan hissalari uchun va albatta bugun sayohat uchun tashakkur izhor qilaman! Boringizga shukr! )`,
+    likeNumber: 130,
+    comentNumber: 43,
+    save: false,
+  },
+
+  {
+    id: 5,
+    title: "Uktamov Bekzod",
+    name: "Najot Talim",
+    media: [
+      "../../img/nature5.jpg",
+      "../../video/first.mp4",
+      "../../img/fifth1.jpg",
+      "../../img/fifth2.jpg",
+    ],
+    desc: `Kuni kecha markazimizning Chilonzor filiali o'quvchilar bilan to'lib-toshdi. <br> Ya'ni hozirda ko'plab yoshlar uchun o'rnak bo'lib kelayotgan inson — Muhammadali Eshonqulov «Najot Ta'lim» markazida mehmon bo'ldi. <br> ✨ Mehmonimiz bilan o'quvchilarimiz o'rtasida samimiy suhbat bo'lib o'tdi. Spikerimiz yoshlarga ruhiyat, nafs tarbiyasi, muvaffaqiyat aslida nima ekanligi kabi bir qator mavzularda o'z maslahat va tavsiyalarini berib o'tdilar. Tadbir so'nggida esa ishtirokchilar bilan savol-javob bo'lib o'tdi. <br> Ushbu suhbat barcha uchun manfaatli bo'lganligiga ishonamiz. Tashrif uchun o'zining qimmatli vaqtini ayamaganligi, bilim va tajribalarini yoshlar bilan bo'lishganligi uchun Muhammadali Eshonqulovga jamoamiz nomidan samimiy minnatdorchilik bildiramiz. 😊`,
+    likeNumber: 15,
+    comentNumber: 5,
+    save: false,
+  },
+
+  {
+    id: 6,
+    title: "Najot Talim",
+    name: "Najot Talim",
+    media: [
+      "../../img/nature6.webp",
+      "../../video/first.mp4",
+      "../../img/six1.jpg",
+    ],
+    desc: `Xabaringiz bor, Najot Ta'lim markazida 5-mart sanasidan boshlab, haftaning har shanba kuni dizayn sohasining bo'lajak vakillarini saralab olish uchun dizayn foundation bosqichi imtihonlari bo'lib o'tdi. <br> Jumladan, bugun ham imtihonlar tashkil etildi. 🚀 Kirish imtihonlarida o'quvchilarning grafik dizayn, mantiq va ingliz tili bo'yicha tuzilgan savollarga bergan javoblari bir necha bosqichli tekshiruvlardan o'tkazildi. <br> Dizayn foundation bosqichi uchun ushbu davrgacha umumiy hisobda 577 nafar nomzodlar ro'yxatdan o'tgan bo'lib, ularning 228 nafari kirish imtihonlarida qatnashishdi. Ulardan 151 nafari esa o'qish uchun qabul qilindi. <br> 📌 Kirish imtihonlaridan muvaffaqiyatli o'tgan barcha o'quvchilar 2-aprel kuniga qadar markazimiz bilan shartnoma tuzishlari talab etiladi.`,
+    likeNumber: 65,
+    comentNumber: 3,
+    save: false,
+  },
+
+  {
+    id: 7,
+    title: "Shaxzod Muradov",
+    name: "Najot Talim",
+    media: [
+      "../../video/fourth.mp4",
+      "../../img/nature7.jpg",
+      "../../img/seven1.jpg",
+      "../../img/seven2.jpg",
+    ],
+    desc: `🎯 Hozirgi kunda «Opera branding agency»da grafik dizayner bo'lib faoliyat olib borayotgan Sevara ismli bitiruvchimiz bilan tanishing.<br> Sevara o'zida sohaga bo'lgan qiziqishni sezganidan so'ng kompyuter uchun pul yig'ishni boshlagani, 800 nafar nomzodlar ichidan «Najot Ta'lim»da o'qish uchun grant yutganligi va karantin davridagi o'qish davri haqida hikoya qilib berdi. <br>Agar siz ham zamonaviy kasblardan birini o'rganishni istasangiz, izohlarda ➕ belgisini qoldiring!`,
+    likeNumber: 123,
+    comentNumber: 43,
+    save: false,
+  },
+
+  {
+    id: 8,
+    title: "Elyor Shukurov",
+    name: "Najot Talim",
+    media: [
+      "../../img/nature8.jpg",
+      "../../video/first.mp4",
+      "../../img/eight.jpg",
+    ],
+    desc: `"Osmondagi bolalar" loyihasining yorqin mehmoni, "Yuksalish" liderlar akademiyasi asoschisi – Muhammadali Eshonqulov bilan master-klass bo'lib o'tdi. Suhbatda yoshlar uchun muhim hisoblangan mavzular: ilm olish, muvaffaqiyatga erishish yo'llari va ruhiyat haqida so'z bordi. Bundan tashqari, o'quvchilarimiz o'zlarini qiziqtirgan savollar bo'yicha mehmonimiz tomonidan javob oldilar. Tez orada ushbu suhbat premyerasini «Najot Ta'lim» YouTube sahifasi orqali tomosha qilishingiz mumkin!`,
+    likeNumber: 460,
+    comentNumber: 102,
+    save: false,
+  },
+
+  {
+    id: 9,
+    title: "Xurshid",
+    name: "Najot Talim",
+    media: [
+      "../../video/third.mp4",
+      "../../img/nature1.jpg",
+      "../../img/nine1.jpg",
+    ],
+    desc: `Yodingizda bo'lsa, avvalroq grafik dizaynerlar o'qishi kerak bo'lgan kitoblar to'plamini tavsiya qilgan edik. <br>Ushbu postning izohlar qismida kuzatuvchilarimiz tomonidan dasturchilar uchun ham kitob tavsiya qilishimiz so'ralgan edi. Obunachilarimizning fikrlarini inobatga olgan holda, bu gal dasturchilar o'qishi kerak bo'lgan kitoblar to'plamini tavsiya qilishga qaror qildik. <br>Ushbu kasb egalari uchun o'z sohalariga doir ushbu kitoblar foydali bo'ladi, degan umiddamiz. 😊P. S. Sizlar uchun yana qanday kontentlar qo'yib borsak, manfaatli bo'ladi deb o'ylaysiz? <br> Qimmatli fikringizni izohlarda qoldiring. 😉`,
+    likeNumber: 305,
+    comentNumber: 78,
+    save: false,
+  },
+];
+
+let ul = document.querySelector(".ul");
+let modal = document.querySelector(".modal");
+let modalBack = document.querySelector(".modal-back");
+
+// ?SHoving objects
+arr.forEach((item) => {
+  if (item.media[0].indexOf(".mp4") !== -1) {
+    ul.innerHTML += `
+      <li class='li' onclick='modalShow(${item.id})'>
+      <div class='img-video-flex'>
+      <video class='first-video' src=${item.media[0]}></video>
+      <img class='first-img' src=${item.media[2]}>
+      <i class='bx bx-play play'></i>
+      </div>
+      
+        <div class='div-back'>
+        <i class='bx  bx-play play2'>${item.likeNumber}</i>
+        <i class='bx bx-message-rounded-add'>${item.comentNumber}</i>
+        </div>
+      </li>
+    `;
+  } else {
+    ul.innerHTML += `
+      <li class='li' onclick='modalShow(${item.id})'>
+      <div class='img-video-flex'>
+        <img  class='first-img' src=${item.media[0]}>
+        <img class='first-img' src=${item.media[2]}>
+        <i class='bx bx-windows windows'></i>
+      </div>
+        <div class='div-back'>
+        <i class='bx bx-heart'>${item.likeNumber}</i>
+        <i class='bx bx-message-rounded-add'>${item.comentNumber}</i>
+        </div>
+      </li>
+    `;
+  }
+});
+
+// ?Modal Show
+function modalShow(id) {
+  modal.classList.add("modal-show");
+  modalBack.classList.add("modal-back-show");
+
+  let modalInArr = arr.filter((item) => {
+    return id === item.id;
+  });
+  modalInArr.forEach((item) => {
+    if (item.media[0].indexOf(".mp4") !== -1) {
+      let divMod = document.createElement("div");
+      divMod.classList.add("modal-in-div");
+      divMod.innerHTML = `
+      <div class='left-img'>
+      <div class='img-video-flex2'>
+      <video autoplay controls class='second-video' src=${item.media[0]}></video>
+        <img class='second-img' src=${item.media[2]}>
+      </div>
+      <button class='btn prev' onclick='moveLeft()' ><i class='bx bx-left-arrow-alt'></i></button>
+      <button class='btn next' onclick='moveRight()'><i class='bx bx-right-arrow-alt' ></i></button>
+    </div>
+    
+    <div class='right-desc'>
+    <div class='right-desc-in'>
+      <div class='image-flex'>
+      <div class='back-circle'>
+        <img class='desc-img' src = '../../img/najot.jpg' alt='img'>
+      </div>
+        
+        <h4 class='image-flex-title'>${item.title}</h4>
+        <i class='bx bx-wifi-0'></i>
+        <a class='follow' href='#'>Follow</a>
+        </div>
+      <i class='bx bx-dots-horizontal-rounded'></i>
+    </div>
+    <hr class='hr'>
+    <div class='flex-img3'>
+      <div class='back-circle'> 
+        <img class='desc-img' src = '../../img/najot.jpg' alt='img'>
+      </div>
+      <p class='modal-desc'>${item.desc}</p>
+    </div>
+    <div class='main-desc-absolute'>
+      <div class='desc-absolute-flex'>
+        <div class='desc-absolute-left'>
+          <i class='bx bx-heart heart-for' onclick='loveClicked(${item.id})'></i>
+          <i class='bx bx-message-rounded-dots'></i>
+          <i class='bx bx-paper-plane'></i>
+        </div>
+        <i class='bx bx-bookmark book' onclick='saveToSaved(${item.id})'></i>
+      </div>
+      <h4 class='like-h4'>${item.likeNumber} likes</h4>
+    </div>
+      
+    </div>
+      `;
+      modal.appendChild(divMod);
+    } else {
+      let divMod = document.createElement("div");
+      divMod.classList.add("modal-in-div");
+      divMod.innerHTML = `
+        <div class='left-img'>
+          <div class='img-video-flex2' id='one'>
+            <img class='second-img' src=${item.media[0]}>
+            <img class='second-img' src=${item.media[2]}>
+          </div>
+          <button class='btn prev' onclick='moveLeft()' ><i class='bx bx-left-arrow-alt'></i></button>
+          <button class='btn next' onclick='moveRight()'><i class='bx bx-right-arrow-alt' ></i></button>
+        </div>
+        
+        <div class='right-desc'>
+          <div class='right-desc-in'>
+            <div class='image-flex'>
+              <div class='back-circle'>
+                <img class='desc-img' src = '../../img/najot.jpg' alt='img'>
+              </div>
+              <h4 class='image-flex-title'>${item.title}</h4>
+              <i class='bx bx-wifi-0'></i>
+              <a class='follow' href='#'>Follow</a>
+            </div>
+            <i class='bx bx-dots-horizontal-rounded'></i>
+          </div>
+          <hr class='hr'>
+        <div class='flex-img3'>
+          <div class='back-circle'>
+            <img class='desc-img' src = '../../img/najot.jpg' alt='img'>
+          </div>
+          <p class='modal-desc'>${item.desc}</p>
+        </div>
+        <div class='main-desc-absolute'>
+      <div class='desc-absolute-flex'>
+        <div class='desc-absolute-left'>
+          <i class='bx bx-heart heart-for' onclick='loveClicked(${item.id})'></i>
+          <i class='bx bx-message-rounded-dots'></i>
+          <i class='bx bx-paper-plane'></i>
+        </div>
+        <i class='bx bx-bookmark book' onclick='saveToSaved(${item.id})'></i>
+      </div>
+      <h4 class='like-h4'>${item.likeNumber} likes</h4>
+    </div>
+          
+        </div>
+      `;
+      modal.appendChild(divMod);
+    }
+  });
+
+  // ?SLider move
+  let imgVideoFlex2 = document.querySelectorAll(".qwer");
+
+  function moveRight() {
+    console.log(imgVideoFlex2);
+  }
+}
+
+// Hiding Modal window
+modalBack.addEventListener("click", () => {
+  modal.classList.remove("modal-show");
+  modalBack.classList.remove("modal-back-show");
+  modal.innerHTML = "";
+});
+
+// Slider Move
+function moveRight() {
+  let imgFlex = document.querySelector(".img-video-flex2");
+  imgFlex.classList.add("img-video-flex2-move-right");
+}
+
+function moveLeft() {
+  let imgFlex = document.querySelector(".img-video-flex2");
+  imgFlex.classList.remove("img-video-flex2-move-right");
+}
+
+// ?Swiper JS
+const swiper = new Swiper(".swiper", {
+  // ?Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // ?If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+    // type: 'progressbar',
+  },
+
+  // ?Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // ?And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 4,
+    },
+    990: {
+      slidesPerView: 6,
+    },
+
+    1100: {
+      slidesPerView: 7,
+    },
+  },
+  slidesPerView: 7,
+  spaceBetween: 20,
+});
+
+// ?Clicking the love
+let count = 0;
+function loveClicked(idTop) {
+  count++;
+  let heart = document.querySelector(".heart-for");
+  let likeH4 = document.querySelector(".like-h4");
+  arr.forEach((ar) => {
+    let like = ar.likeNumber;
+    if (idTop == ar.id && ar.save == false) {
+      likeH4.innerHTML = like + 1 + " likes";
+      heart.classList.toggle("heart-for-clicked");
+    }
+  });
+}
+
+// ?Working with bookmark
+let ul2 = document.querySelector(".saved-messages-ul");
+let savedArr = [];
+function saveToSaved(idSave) {
+  let bookMark = document.querySelector('.book')
+  bookMark.classList.toggle('bokmark-clicked')
+  arr.forEach((item) => {
+    if (idSave === item.id) {
+      savedArr.push(item);
+      savedArr.forEach((saved) => {
+        if (item.media[0].indexOf(".mp4") !== -1) {
+          ul2.innerHTML += `
+          <li class='li' onclick='modalShow(${saved.id})'>
+          <div class='img-video-flex'>
+          <video class='first-video' src=${saved.media[0]}></video>
+          <img class='first-img' src=${saved.media[2]}>
+          <i class='bx bx-play play'></i>
+          </div>
+          
+            <div class='div-back'>
+            <i class='bx  bx-play play2'>${saved.likeNumber}</i>
+            <i class='bx bx-message-rounded-add'>${saved.comentNumber}</i>
+            </div>
+          </li>
+        `;
+        } else {
+          ul2.innerHTML += `
+          <li class='li' onclick='modalShow(${saved.id})'>
+          <div class='img-video-flex'>
+            <img  class='first-img' src=${saved.media[0]}>
+            <img class='first-img' src=${saved.media[2]}>
+            <i class='bx bx-windows windows'></i>
+          </div>
+            <div class='div-back'>
+            <i class='bx bx-heart'>${saved.likeNumber}</i>
+            <i class='bx bx-message-rounded-add'>${saved.comentNumber}</i>
+            </div>
+          </li>
+        `;
+        }
+        savedArr = [];
+      });
+    }
+  });
+}
+
+// ?Clicking saved
+function savedMes() {
+  ul.classList.add("ul-hide");
+  ul2.classList.add("saved-messages-ul-show");
+}
+
+// ?Clicking Post
+function PostShow() {
+  ul.classList.remove("ul-hide");
+  ul2.classList.remove("saved-messages-ul-show");
+}
